d1de91e5da3e4cc45f2f660a6283ec1c3306d7aa
DraganTashevHaaland <60400101+DraganTashevHaaland@users.noreply.github.com>
2020-03-20 19:12:27 +0100

    Add files via upload

---
+++ ProgramLoopsArrays.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LoopsArraysHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int i, even = 0;
+			int[] arr = new int[5];
+
+			Console.Write("Enter Five Numbers :");
+
+			for (i = 0; i < arr.Length; i++)
+			{
+				arr[i] = Convert.ToInt32(Console.ReadLine());
+			}
+
+			for (i = 0; i < 5; i++)
+			{
+				if (arr[i] % 2 == 0)
+				{
+					even = even + arr[i];
+				}
+				
+				
+			}
+			Console.WriteLine("Sum of even numbers is :" + even);
+			
+		}
+    }
+}

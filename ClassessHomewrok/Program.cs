3427f28a9ecd4316befcaf8c1155032e0b04dfc1
DraganTashevHaaland <60400101+DraganTashevHaaland@users.noreply.github.com>
2020-04-02 12:37:24 +0200

    Add files via upload

---
+++ ClassessHomewrok/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ClassesHomework
+{
+    public class Dog
+    {
+          //public Dog(string name, string race, string color)
+          //{
+          //  Name = name;
+          //  Race = race;
+          //  Color = color;
+          //}
+          public string Name { get; set; }
+          public string Race { get; set; }
+          public string Color { get; set; } 
+        
+        public void Eating()
+        {
+            Console.WriteLine("The dog with name " + Name + " ,is now eating");
+        }
+
+        public void Playing()
+        {
+            Console.WriteLine("The dog with breed " + Race + " is now playing");
+        }
+
+        public void ChaseTail()
+        {
+            Console.WriteLine("The dog with name " + Name + " , also with breed " + Race + " and color " + Color + " , is now chasing his tail.");
+        }
+    }
+    class Porgram
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+
+            dog.Name = "Cesar";
+            dog.Race = "labradour";
+            dog.Color = "white";
+
+            dog.Eating();
+            dog.Playing();
+            dog.ChaseTail();
+        }
+    }
+
+   
+    
+}
+

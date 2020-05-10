8e57ff181f516dcc6b956832edcc36a45f224500
DraganTashevHaaland <60400101+DraganTashevHaaland@users.noreply.github.com>
2020-05-10 20:24:55 +0200

    Add files via upload

---
+++ Inheritans/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Domain;
+
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.Name = "Dragan";
+            employee.Surname = "Tashev";
+            employee.Salary = 8000;
+
+            Manager manager = new Manager("Dragan", "Tashev", 8000, Domain.Enums.Role.Manager);
+
+
+            Console.ReadLine();
+            
+        }
+        
+    }
+}
